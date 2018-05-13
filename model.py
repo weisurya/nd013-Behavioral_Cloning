fe1affd902d1b18b6144e699f774e5152f6a51b8
Soen, Surya Soenaryo <wei.surya@gmail.com>
2018-05-13 12:57:46 +0700

    Modify model.py

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ with open('./recorded_data/driving_log.csv') as csvfile:
 
 train_samples, validation_samples = train_test_split(samples[1:], test_size=0.2)
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=32, correction=0.2):
     num_samples = len(samples)
     while True: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -27,43 +27,44 @@ def generator(samples, batch_size=32):
 
             images, angles = [], []
             for batch_sample in batch_samples:
-                    name = './data/IMG/' + batch_sample[0].split('/')[-1]
-                    center_image = cv2.imread(name)
-                    center_angle = float(batch_sample[3])
-                    images.append(center_image)
-                    angles.append(center_angle)
-
-                    center_image_flipped = cv2.flip(center_image, 1)
-                    center_angle_flipped = -center_angle
-                    images.append(center_image_flipped)
-                    angles.append(center_angle_flipped)
+                for i in range(3):
+                    # Linux
+                    # name = './data/IMG/' + batch_sample[i].split('/')[-1]
+                    # Windows
+                    name = './recorded_data/IMG/' + batch_sample[i].split('\\')[-1]
+                    image = cv2.imread(name)
+                    angle = float(batch_sample[3])
+                    if i == 1:
+                        angle += correction
+                    elif i == 2:
+                        angle -= correction
+                    images.append(image)
+                    images.append(np.fliplr(image))
+                    angles.append(angle)
+                    angles.append(angle * -1.0)
 
 
             # trim image to only see section with road
-            X_train = np.array(images)
+            x_train = np.array(images)
             y_train = np.array(angles)
-            yield shuffle(X_train, y_train)
+            yield shuffle(x_train, y_train)
 
 # compile and train the model using the generator function
-train_generator      = generator(train_samples, batch_size=32)
+train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
+
+# Dimensions of the images
+ch, row, col = 3, 160, 320
 
 # Initialization
 model = Sequential()
-# Crop the top image which unused
-model.add(Cropping2D(cropping=((60,25), (0,0)), input_shape=(160,320,3)))
-# Normalized the image
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-
-model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(row, col, ch), output_shape=(row, col, ch)))
+model.add(Cropping2D(cropping=((60, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
-# model.add(Convolution2D(6, 5, 5, subsample=(2,2), activation='relu'))
-# model.add(MaxPooling2D())
-# model.add(Convolution2D(6, 5, 5, subsample=(2,2), activation='relu'))
-# model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -71,9 +72,8 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
 history_object = model.fit_generator(train_generator,
-                                     samples_per_epoch = len(train_samples),
+                                     samples_per_epoch = len(train_samples) * 6, #data augmentation
                                      validation_data = validation_generator,
                                      nb_val_samples = len(validation_samples),
                                      nb_epoch=10)
@@ -91,5 +91,5 @@ plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+# plt.show()
 plt.savefig('loss.jpg')
